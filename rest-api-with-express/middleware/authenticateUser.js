b6ba1fd51b63895e0b43674a47073ae8150b898d
alecstephens <stephens.alec01@gmail.com>
2022-05-12 14:30:19 -0500

    created and finished the middlware to help authenticate a user

---
+++ rest-api-with-express/middleware/authenticateUser.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const auth = require('basic-auth');
+const { User } = require('../models');
+const bcrypt = require('bcrypt');
+
+exports.authenticateUser = async (req, res, next) => {
+    let message;
+    const credentials = auth(req);
+
+
+    // If the credentials are available
+    if(credentials) {
+        const user = await User.findOne({ where: {emailAddress: credentials.name} });
+        if(user) {
+            const authenticated = bcrypt
+                .compareSync(credentials.pass, user.password);
+            if(authenticated) {
+                console.log(`Authentication successfull for user: ${user.emailAddress}`);
+                req.currentUser = user;
+            } else {
+                message = `Authentication failed for user: ${user.emailAddress}`;
+            }
+        } else {
+            message: `User ${credentials.name} was not found`;
+        }
+    } else {
+        message: `Login required for this route`;
+    }
+    if(message) {
+        console.warn(message);
+        res.status(401).json({ message: 'Access Denied' })
+    }
+    next();
+}
