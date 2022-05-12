7ca86f918dba49b9fcf774a9d3bfd8703c843e8e
alecstephens <stephens.alec01@gmail.com>
2022-05-12 14:29:55 -0500

    pretty much started and finished routes.js file

--- rest-api-with-express/routes.js
+++ rest-api-with-express/routes.js
@@ -1,7 +1,22 @@
 const express = require('express');
-const { async } = require('seed/lib/seed');
 const { User } = require('./models');
 const { Course } = require('./models');
+const {authenticateUser} = require('./middlware/authenticateUser');
+const { async } = require('seed/lib/seed');
+
+const router = express.Router;
+
+// asyncHandler function to wrap each route/middleware
+function asyncHandler(cb) {
+    return async(req, res, next) => {
+        try {
+            await cb(req, res, next)
+        } catch(error) {
+            res.status(500).send(error);
+            next(error);
+        }
+    }
+}
 
 // User Routes
 // A route that will GET all properties and values for the user with a 200 status code
@@ -28,3 +43,99 @@ router.post('./users', asyncHandler(async (req, res) =>{
         }
     }
 }));
+
+// Course Routes
+// A route that will GET the courses with user and return a 200 status code
+router.get('./course', asyncHandler(async (req, res) => {
+    const courses = await Course.findAll({
+        attributes: [ 'title', 'description', 'estimatedTime', 'materialsNeeded', 'userId' ],
+        include: [
+            {
+                model: User,
+                attributes: [ 'firstName', 'lastName', 'emailAddress' ]
+            }
+        ]
+    });
+    res.status(200).json(course);
+}))
+
+// A route that will create a new course
+router.post('./courses', authenticateUser, asyncHandler(async (req, res) => {
+    try {
+        let course = await Course.create(req.body);
+        res.location(`/courses/${course.id}`).status(201).end();
+    } catch(error) {
+        if(error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+            const errors = error.errors.map(error => error.message);
+            res.status(400).json({ errors });
+        } else {
+            throw error;
+        }
+    }
+}));
+
+// A route that will return a specific course and user
+router.get('./courses/:id', asyncHandler(async (req, res) => {
+    const courses = await Course.findOne({
+        attributes: [ 'title', 'description', 'estimatedTime', 'materialsNeeded', 'userId'],
+        include: [
+            {
+                model: User,
+                attributes: [ 'firstName', 'lastName', 'emailAddress' ]
+            }
+        ]
+    });
+    res.status(200).json(course);
+}))
+
+// A route that will update the corresponding course and return a 204 HTTP status code and no content
+router.put('./course/:id', authenticateUser, asyncHandler(async (req, res) => {
+    try {
+        let course = await Course.findOne({ id: req.params.id });
+        const user = req.currentUser;
+        if(course) {
+            if(user.id === course.userId) {
+                await course.update(req.body);
+                res.status(204).end();
+            } else {
+                res.status(403).json({ error: {message: 'Only users associated with this course can make changes'}});
+            }
+        } else {
+            res.status(404).json({ error: {message: `Course not found with id of ${req.params.id}`}});
+        }
+    } catch(error) {
+        if(error.name === 'SequelizeValidationError') {
+            const errors = error.errors.map(error => error.message);
+            res.status(400).json({ errors });
+        } else {
+            throw error;
+        }
+    }
+}));
+
+// A route that will delete a specific course 
+router.delete('./courses/:id', authenticateUser, asyncHandler(async (req, res) => {
+    try {
+        let course = await Course.findOne({ where: { id: req.params.id } });
+        const user = req.currentUser;
+        if(course) {
+            if(user.id === course.userId) {
+                await course.destroy();
+                res.status(204).end();
+            } else {
+                res.status(403).json({error: {message: 'Only users associated witht his course can make changes'}});
+            }
+        } else {
+            res.status(404).json({error: {message: `Course not found with id of ${req.params.id}`}});
+        }
+    } catch(error) {
+        if(error.name === 'SequelizeValidationError') {
+            const errors = error.errors.map(error => error.message);
+            res.status(400).json({ errors });
+        } else {
+            throw error;
+        }
+    }
+}));
+
+module.exports = router;
